75f934162489691f0685f77147ccc5e4e25a9555
Moohaeng Sohn <alexsohn1126@gmail.com>
2024-01-19 12:32:53 -0500

    Added Sketches + code to test how to divide resturants by ward

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ The repo is structured as:
 -   `outputs/data` contains the cleaned dataset that was constructed.
 -   `outputs/paper` contains the files used to generate the paper, including the Quarto document and reference bibliography file, as well as the PDF of the paper. 
 -   `scripts` contains the R scripts used to simulate, download and clean data.
+
+## LLM Usage
+
+No LLM was used in the process of writing this report.
--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -11,16 +11,34 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(sf)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# download ward geojson data (to convert lat-long coordinates to wards)
+map_ids <- list_package_resources("5e7a8234-f805-43ac-820f-03d7c360b588") |> 
+  filter(tolower(format) %in% c('geojson')) |>
+  pull(id)
+
+# download dinesafe data
+dinesafe_ids <- list_package_resources("ea1d6e57-87af-4e23-b722-6c1f5aa18a8d") |> 
+  filter(tolower(format) %in% c('csv')) |>
+  pull(id)
+
+# get resources
+ward_map_data <- get_resource(map_ids[1])
+dinesafe_data <- get_resource(dinesafe_ids[2])
+
+# plot map with all the dinesafe restaurants TODO: COMMENT THIS OUT OR DELETE BEFORE SUBMITTING
+ggplot(ward_map_data) +
+  geom_sf() +
+  geom_point(data = dinesafe_data, aes(x=Longitude, y=Latitude))
+
 
 
 
 #### Save data ####
 # [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+#write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
          
