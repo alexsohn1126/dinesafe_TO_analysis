025ad5a05bb90e22aed5307bc0bee68ee080f384
Moohaeng Sohn <alexsohn1126@gmail.com>
2024-01-19 13:44:05 -0500

    Created simulated data and change dataset sketch

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -10,10 +10,27 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+# set seed
+set.seed(302)
+simulated_data <- tibble(
+  ward = 1:25,
+  ward_income = sample(1:100000, 25, replace = TRUE),
+  dinesafe_infraction_ratio = rbeta(25, shape1 = 2, shape2 = 6)
+)
 
+#### Test data ####
+# Type check
+simulated_data$ward |> is.numeric()
+simulated_data$ward_income |> is.numeric()
+simulated_data$dinesafe_infraction_ratio |> is.numeric()
 
+# Bound check
+simulated_data$ward |> min() >= 1
+simulated_data$ward_income |> min() >= 0
+simulated_data$dinesafe_infraction_ratio |> min() >= 0
+
+simulated_data$ward |> max() <= 25  # Only 25 wards in Toronto
+simulated_data$dinesafe_infraction_ratio |> max() <= 1
 
